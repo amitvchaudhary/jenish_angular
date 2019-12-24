3bfe6adb19f4929cf01c540d6e32d96abb5639b8
jenish.soni <jenish.soni@azilen.com>
2019-12-24 12:26:37 +0530

    added new component

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewHouseComponent } from './new-house/new-house.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewHouseComponent
   ],
   imports: [
     BrowserModule,
