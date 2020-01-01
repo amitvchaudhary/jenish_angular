32d7611d4e7f23b8e6f8b0534cb7ea0e0027cada
jenish.soni <jenish.soni@azilen.com>
2020-01-01 16:27:57 +0530

    added car-list component and reused the array

--- my-first-app/src/app/car-list/car-list.component.ts
+++ my-first-app/src/app/car-list/car-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Car } from '../core/car';
 
 @Component({
   selector: 'app-car-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car-list.component.css']
 })
 export class CarListComponent implements OnInit {
+
+  @Input() carList: Car[] = [];
 
   constructor() { }
 
